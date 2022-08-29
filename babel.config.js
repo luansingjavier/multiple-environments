cef21cfa8e07c3cd774484df18691e7cbc0c804f
luansingjavier <luansingjavier@gmail.com>
2022-08-29 23:33:53 +0800

    chore: add multiple environments

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
+};
